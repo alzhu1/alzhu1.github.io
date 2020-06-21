ac003cc6dfd08a6eda9284818997fd30ee341dc1
Alex Zhu <agz003@ucsd.edu>
2020-06-21 01:24:01 -0700

    Updates

---
+++ precache-manifest.96f7e8cf12293539b3b073208bea1ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b099159a31fbc5952e370c6a8c763e0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aee169f8c980ad41476c",
+    "url": "/static/css/main.b14ed6b8.chunk.css"
+  },
+  {
+    "revision": "7db6d446660f199ecfe1",
+    "url": "/static/js/2.0af57b2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0af57b2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aee169f8c980ad41476c",
+    "url": "/static/js/main.290a3ddb.chunk.js"
+  },
+  {
+    "revision": "c122a9f7b9fcc17b018f",
+    "url": "/static/js/runtime-main.d392aba1.js"
+  }
+]);
